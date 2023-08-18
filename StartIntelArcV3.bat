3ee8c91266a4bd8389078f62485b1db4b7998574
拉拉姬 <sa@iamverycute.com>
2023-08-18 20:21:24 +0300

    v3

--- StartIntelArcV3.bat
+++ StartIntelArcV3.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 SET ARC_PATH="C:\Program Files\Intel\Intel Arc Control\ArcControl.exe"
-POWERSHELL -WindowStyle HIDDEN -Command "& Stop-Process -Name ArcControl -Force; Stop-Service IntelArcControlService; Start-Process ""%ARC_PATH%""; Start-Sleep -Seconds 3; Start-Service IntelArcControlService"
+POWERSHELL -WindowStyle Hidden -Command "& Get-Process -Name "ArcControlService","ArcControl" | Stop-Process -Force; Start-Process ""%ARC_PATH%""; Start-Sleep 3; Start-Service IntelArcControlService
